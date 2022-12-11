47026daa1667173085711b332d3f5729aeecdd2e
Marat Mingazov <marat_mingazov@epam.com>
2022-12-11 20:43:36 +0300

    Implement getBuckets controller

--- src/main/java/maratmingazovr/aws_ms/controller/AwsController.java
+++ src/main/java/maratmingazovr/aws_ms/controller/AwsController.java
@@ -2,33 +2,48 @@ package maratmingazovr.aws_ms.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import lombok.val;
+import maratmingazovr.aws_ms.dto.AwsBucketDto;
 import maratmingazovr.aws_ms.dto.CreateBucketRequestDTO;
 import maratmingazovr.aws_ms.service.aws.S3Service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Log4j2
 @RestController
 @AllArgsConstructor
-@RequestMapping("/aws")
+@RequestMapping("/buckets")
 public class AwsController {
 
     private final S3Service s3Service;
 
-    @PostMapping("/create_bucket")
+    @PostMapping
     public ResponseEntity<Void> createBucket(@RequestBody CreateBucketRequestDTO request) {
         s3Service.createBucket(request.getBucketName());
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/delete_bucket")
+    @DeleteMapping
     public ResponseEntity<Void> deleteBucket(@RequestParam String bucketName) {
         s3Service.deleteBucket(bucketName);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AwsBucketDto>> getBuckets() {
+        val buckets = s3Service.getBuckets();
+        val bucketsDto = buckets.stream()
+                .map(AwsBucketDto::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(bucketsDto);
     }
 }
