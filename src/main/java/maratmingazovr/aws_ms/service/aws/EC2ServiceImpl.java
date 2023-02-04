f4dc42ee945aa515e716736f0a5a1cc712c63232
Marat Mingazov <marat_mingazov@epam.com>
2023-02-04 21:32:35 +0300

    Catch AwsSdkException

--- src/main/java/maratmingazovr/aws_ms/service/aws/EC2ServiceImpl.java
+++ src/main/java/maratmingazovr/aws_ms/service/aws/EC2ServiceImpl.java
@@ -3,6 +3,7 @@ package maratmingazovr.aws_ms.service.aws;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import maratmingazovr.aws_ms.exception.AwsSdkException;
 import maratmingazovr.aws_ms.model.aws.AwsInstanceInfo;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.regions.internal.util.EC2MetadataUtils;
@@ -15,6 +16,11 @@ public class EC2ServiceImpl implements EC2Service {
     @NonNull
     @Override
     public AwsInstanceInfo getInstanceInfo() {
-        return new AwsInstanceInfo(EC2MetadataUtils.getAvailabilityZone());
+        try {
+            return new AwsInstanceInfo(EC2MetadataUtils.getAvailabilityZone());
+        } catch (Exception e) {
+            throw new AwsSdkException(String.format("EC2ServiceException: Unable get instanceInfo. '%s'", e.getMessage()), e);
+        }
+
     }
 }
