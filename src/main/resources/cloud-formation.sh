b4a9fd31cfc96544e3a90a1853bf5d981596a9b7
Marat Mingazov <marat_mingazov@epam.com>
2023-02-22 12:12:57 +0300

    Create cloud formation script

---
+++ src/main/resources/cloud-formation.sh
@@ -0,0 +1,13 @@
+aws cloudformation create-stack \
+    --stack-name WebServerVPCStack \
+    --template-url https://maratmingazovr.s3.amazonaws.com/VPC.template \
+    --region us-east-1 \
+    --parameters \
+        ParameterKey=PrivateSubnet1CIDR,ParameterValue=10.0.10.0/24 \
+        ParameterKey=PrivateSubnet2CIDR,ParameterValue=10.0.11.0/24 \
+        ParameterKey=PublicSubnet1CIDR,ParameterValue=10.0.0.0/24 \
+        ParameterKey=PublicSubnet2CIDR,ParameterValue=10.0.1.0/24 \
+        ParameterKey=VPCCIDR,ParameterValue=10.0.0.0/16
+
+
+aws cloudformation delete-stack --stack-name WebServerVPCStack
