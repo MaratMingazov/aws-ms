3e3099d114f3e04b4157adb23ef5c491f352e059
Marat Mingazov <marat_mingazov@epam.com>
2023-03-16 10:21:59 +0300

    Set test properties

--- src/test/java/maratmingazovr/aws_ms/ApplicationTests.java
+++ src/test/java/maratmingazovr/aws_ms/ApplicationTests.java
@@ -6,6 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
+	static {
+		System.setProperty("DB_URL", "jdbc:postgresql://localhost:5432/aws_ms_db");
+		System.setProperty("DB_USERNAME", "postgres");
+		System.setProperty("DB_PASSWORD", "password");
+	}
+
 	@Test
 	void contextLoads() {
 	}
